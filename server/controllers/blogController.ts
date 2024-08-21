6c2d9ee41a03a9e2d190298f774e027a66b4e60d
Swarnab <swarnabmessibanerjee@gmail.com>
2024-08-21 21:50:50 +0530

    Backend Completed

--- server/controllers/blogController.ts
+++ server/controllers/blogController.ts
@@ -1,6 +1,6 @@
 import fs from "fs";
 import jwt, { JwtPayload } from "jsonwebtoken";
-import {Request, Response } from "express";
+import { Request, Response } from "express";
 import BlogModel from "../models/blogModel";
 
 // Type for the JWT payload
@@ -64,14 +64,13 @@ export const createBlog = async (req, res: Response) => {
       });
 
       // Respond with the created blog document
-      res.json(blogDoc);
+      res.status(200).json({ success: true, data: blogDoc });
     });
   } catch (error) {
     console.error("Error in uploadPic:", error);
     res.status(500).json({ success: false, message: "Internal server error" });
   }
 };
-
 
 // update blog post
 export const updateBlog = async (req, res: Response) => {
@@ -80,7 +79,7 @@ export const updateBlog = async (req, res: Response) => {
 
     if (req.file) {
       const { originalname, path } = req.file;
-      const parts = originalname.split('.');
+      const parts = originalname.split(".");
       const ext = parts[parts.length - 1];
       newPath = `${path}.${ext}`;
       fs.renameSync(path, newPath);
@@ -91,7 +90,7 @@ export const updateBlog = async (req, res: Response) => {
 
     jwt.verify(token, secret, {}, async (err, decodedToken) => {
       if (err || !decodedToken) {
-        return res.status(401).json('Unauthorized or invalid token');
+        return res.status(401).json("Unauthorized or invalid token");
       }
 
       const info = decodedToken as CustomJwtPayload;
@@ -100,13 +99,13 @@ export const updateBlog = async (req, res: Response) => {
       const blog = await BlogModel.findById(id);
 
       if (!blog) {
-        return res.status(404).json('Blog post not found');
+        return res.status(404).json("Blog post not found");
       }
 
       const isAuthor = JSON.stringify(blog.author) === JSON.stringify(info.id);
 
       if (!isAuthor) {
-        return res.status(403).json('You are not the author');
+        return res.status(403).json("You are not the author");
       }
 
       // Update the blog post
@@ -121,6 +120,86 @@ export const updateBlog = async (req, res: Response) => {
     });
   } catch (error) {
     console.error("Error in updateBlog:", error);
+    res.status(500).json({ success: false, message: "Can not update Blog" });
+  }
+};
+
+// get all blog
+export const getAllBlog = async (req: Request, res: Response) => {
+  try {
+    const singleBlog = await BlogModel.find()
+      .populate("author", ["username"])
+      .sort({ createdAt: -1 })
+      .limit(20);
+
+    res.status(200).json({ success: true, message: "Ok", data: singleBlog });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ success: false, message: "Could not get all blogs" });
+  }
+};
+
+// get single blog post
+export const getSingleBlog = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const singleBlog = await BlogModel.findById(id).populate("author", [
+      "username",
+    ]);
+
+    if (!singleBlog) {
+      return res.status(404).json("Blog post not found");
+    }
+
+    res.status(200).json({ success: true, message: "Ok", data: singleBlog });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ success: false, message: "Could not get a single blog" });
+  }
+};
+
+// delete blog post
+export const deleteBlog = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const { token } = req.cookies;
+    const secret = process.env.JWT_SECRET as string;
+
+    jwt.verify(token, secret, {}, async (err, decodedToken) => {
+      if (err || !decodedToken) {
+        return res
+          .status(401)
+          .json({ success: false, message: "Unauthorized or invalid token" });
+      }
+
+      const info = decodedToken as CustomJwtPayload;
+      const blog = await BlogModel.findById(id);
+
+      if (!blog) {
+        return res
+          .status(404)
+          .json({ success: false, message: "Blog post not found" });
+      }
+
+      const isAuthor = JSON.stringify(blog.author) === JSON.stringify(info.id);
+
+      if (!isAuthor) {
+        return res
+          .status(403)
+          .json({ success: false, message: "You are not the author" });
+      }
+
+      // Delete the blog post
+      await BlogModel.deleteOne({ _id: blog._id });
+
+      res
+        .status(200)
+        .json({ success: true, message: "Blog post deleted successfully" });
+    });
+  } catch (error) {
+    console.error("Error in deleteBlog:", error);
     res.status(500).json({ success: false, message: "Internal server error" });
   }
 };
--- server/route/blogRoutes.ts
+++ server/route/blogRoutes.ts
@@ -1,9 +1,19 @@
-import express from 'express';
-import { createBlog } from '../controllers/blogController';
-import { uploadMiddleware } from '../middleware/upload';
+import express from "express";
+import {
+  createBlog,
+  deleteBlog,
+  getAllBlog,
+  getSingleBlog,
+  updateBlog,
+} from "../controllers/blogController";
+import { uploadMiddleware } from "../middleware/upload";
 
 const router = express.Router();
 
-router.post('/create-blog',uploadMiddleware.single('file') ,createBlog);
+router.post("/create-blog", uploadMiddleware.single("file"), createBlog);
+router.put("/update-blog", uploadMiddleware.single("file"), updateBlog);
+router.get("/get-blogs", getAllBlog);
+router.get("/get-blog/:id", getSingleBlog);
+router.delete("/delete-blog/:id", deleteBlog);
 
 export default router;
